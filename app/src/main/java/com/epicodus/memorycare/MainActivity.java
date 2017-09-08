8b97f0bcea85f4ecbcfbd405673925cf1ae25417
Mara Timberlake <maratimberlake@msn.com>
2017-09-08 15:38:18 -0700

    gather user input from EditText

--- app/src/main/java/com/epicodus/memorycare/MainActivity.java
+++ app/src/main/java/com/epicodus/memorycare/MainActivity.java
@@ -3,22 +3,29 @@ package com.epicodus.memorycare;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindPatientButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
 
         mFindPatientButton = (Button) findViewById(R.id.findPatientButton);
         mFindPatientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, PatientActivity.class);
                 startActivity(intent);
 
