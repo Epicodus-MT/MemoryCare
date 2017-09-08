b5a4dd16025afec77845c2daa86529f80897ef2d
Mara Timberlake <maratimberlake@msn.com>
2017-09-08 14:51:05 -0700

    add Toast message to main layout page

--- app/src/main/java/com/epicodus/memorycare/MainActivity.java
+++ app/src/main/java/com/epicodus/memorycare/MainActivity.java
@@ -1,13 +1,25 @@
 package com.epicodus.memorycare;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindPatientButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindPatientButton = (Button) findViewById(R.id.findPatientButton);
+        mFindPatientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello Elysia!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
