a202c10ba92cb3f2550411fbdb3fb8e0cd621688
Mara Timberlake <maratimberlake@msn.com>
2017-09-09 14:51:13 -0700

    update build.gradle (module:app) with necessary libraries

---
+++ app/src/test/java/com/epicodus/memorycare/MainActivityTest.java
@@ -0,0 +1,50 @@
+package com.epicodus.memorycare;
+
+/**
+ * Created by Mara on 9/9/17.
+ */
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+
+import android.content.Intent;
+import org.robolectric.shadows.ShadowActivity;
+
+
+
+import static junit.framework.Assert.assertTrue;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("MemoryCare".equals(appNameTextView.getText().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted() {
+        activity.findViewById(R.id.findPatientButton).performClick();
+        Intent expectedIntent = new Intent(activity, PatientActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+}
