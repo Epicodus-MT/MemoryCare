49fcc60cc0a469e1d1f6f739eb8e3ed211184e26
Mara Timberlake <maratimberlake@msn.com>
2017-09-17 11:35:33 -0700

    complete third activity to navigate user

--- app/src/main/java/com/epicodus/memorycare/AdviceActivity.java
+++ app/src/main/java/com/epicodus/memorycare/AdviceActivity.java
@@ -1,13 +1,14 @@
 package com.epicodus.memorycare;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 
 /**
  * Created by Mara on 9/17/17.
  */
 
-public class AdviceActivity {
+public class AdviceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
