6752725cc1f6eef0d34fda1cb6fe9f2c1756d7f5
Mara Timberlake <maratimberlake@msn.com>
2017-09-16 18:26:05 -0700

    implement a ListView and ArrayAdapter

--- app/src/main/java/com/epicodus/memorycare/PatientActivity.java
+++ app/src/main/java/com/epicodus/memorycare/PatientActivity.java
@@ -3,18 +3,47 @@ package com.epicodus.memorycare;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PatientActivity extends AppCompatActivity {
     private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] patients = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patients);
+        mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, patients);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String patient = ((TextView)view).getText().toString();
+                Toast.makeText(PatientActivity.this, patient, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the locations visited on: " + location);
+        mLocationTextView.setText("Here are all the locations near: " + location);
     }
 }
+
+
+
---
+++ app/src/test/java/com/epicodus/memorycare/PatientActivityTest.java
@@ -0,0 +1,39 @@
+package com.epicodus.memorycare;
+
+/**
+ * Created by Mara on 9/8/17.
+ */
+
+        import android.os.Build;
+        import android.widget.ListView;
+
+        import org.junit.Before;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+        import org.robolectric.Robolectric;
+        import org.robolectric.RobolectricGradleTestRunner;
+        import org.robolectric.annotation.Config;
+
+        import static junit.framework.Assert.assertEquals;
+        import static junit.framework.Assert.assertNotNull;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class PatientActivityTest {
+    private PatientActivity activity;
+    private ListView mPatientListView;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(PatientActivity.class);
+        mPatientListView = (ListView) activity.findViewById(R.id.listView);
+    }
+
+    @Test
+    public void restaurantListViewPopulates() {
+        assertNotNull(mPatientListView.getAdapter());
+        assertEquals(mPatientListView.getAdapter().getCount(), 15);
+    }
+
+}
