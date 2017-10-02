309ed20431719a74f67b1e8ec60de1b8b7bf61b7
Mara Timberlake <maratimberlake@msn.com>
2017-10-01 21:31:09 -0700

    add logic for Picasso library to handle decoding our images

--- app/src/main/java/com/epicodus/memorycare/ui/FirebasePatientViewHolder.java
+++ app/src/main/java/com/epicodus/memorycare/ui/FirebasePatientViewHolder.java
@@ -1,9 +1,14 @@
 package com.epicodus.memorycare.ui;
 
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,8 +26,9 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
-import java.util.ArrayList;
 
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class FirebasePatientViewHolder extends RecyclerView.ViewHolder {
@@ -31,6 +37,7 @@ public class FirebasePatientViewHolder extends RecyclerView.ViewHolder {
 
     View mView;
     Context mContext;
+    public ImageView mPatientImageView;
 
     public FirebasePatientViewHolder(View itemView) {
         super(itemView);
@@ -52,8 +59,45 @@ public class FirebasePatientViewHolder extends RecyclerView.ViewHolder {
                     .into(patientImageView);
         }
 
+        if (!patient.getImageUrl().contains("http")) {
+            try {
+                Bitmap imageBitmap = decodeFromFirebaseBase64(patient.getImageUrl());
+                mPatientImageView.setImageBitmap(imageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Picasso.with(mContext)
+                    .load(patient.getImageUrl())
+                    .resize(MAX_WIDTH, MAX_HEIGHT)
+                    .centerCrop()
+                    .into(mPatientImageView);
+        }
+
+
         nameTextView.setText(patient.getName());
         categoryTextView.setText(patient.getCategories().get(0));
         ratingTextView.setText("Rating: " + patient.getRating() + "/5");
+    }
+
+    public static Bitmap decodeFromFirebaseBase64(String image) throws IOException {
+        byte[] decodedByteArray = android.util.Base64.decode(image, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedByteArray, 0, decodedByteArray.length);
+    }
+
+    @Override
+    public void onItemSelected() {
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_on);
+        set.setTarget(itemView);
+        set.start();
+    }
+
+    @Override
+    public void onItemClear() {
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_off);
+        set.setTarget(itemView);
+        set.start();
     }
 }
